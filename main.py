630451b0acc5fc9d4120a11d8dd97951e5c9c2c7
hymntaha <tahauygun@gmail.com>
2023-12-26 05:18:30 +0000

    Adding tf gpu

--- main.py
+++ main.py
@@ -19,5 +19,91 @@ tf.config.list_physical_devices('GPU')
 
 import cv2
 import imghdr
+import os
 
-data_dir = 'data'
+data_dir = '/content/drive/MyDrive/data'
+
+image_exts = ['jpeg','jpg','bmp','png']
+
+for image_class in os.listdir(data_dir):
+  for image in os.listdir(os.path.join(data_dir, image_class)):
+    image_path = os.path.join(data_dir, image_class, image)
+    try:
+      img = cv2.imread(image_path)
+      tip = imghdr.what(image_path)
+      if tip not in image_exts:
+        print("Image not in ext list {}".format(image_path))
+        os.remove(image_path)
+    except Exception as e:
+      print('Issue with image {}'.format(image_path))
+
+
+LOAD DATA
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+data = tf.keras.utils.image_dataset_from_directory('/content/drive/MyDrive/data')
+
+data_iterator = data.as_numpy_iterator()
+
+batch = data_iterator.next()
+
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+
+for idx, img in enumerate(batch[0][:4]):
+  ax[idx].imshow(img.astype(int))
+  ax[idx].title.set_text(batch[1][idx])
+
+SCALE DATA
+
+data = data.map(lambda x,y: (x/255, y))
+data.as_numpy_iterator().next()
+
+Split Data
+
+train_size = int(len(data) * .7)
+val_size = int(len(data) * .2)
+test_size = int(len(data) * .1) 
+
+train_size
+
+train = data.take(train_size)
+val = data.skip(train_size).take(val_size)
+test = data.skip(train_size+val_size).take(test_size)
+
+BUILD DEEP LEARNING MODEL
+
+train
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+
+model = Sequential()
+
+
+
+model.add(Conv2D(16, (3,3), 1, activation='relu',input_shape=(256,256,3)))
+model.add(MaxPooling2D())
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+
+model.summary()
+
+TRAIN
+
+logdir='logs'
+
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+
+hist = model.fit(train, epochs=20, validation_data=val, callbacks = [tensorboard_callback])
+
+Plot Performance
+
