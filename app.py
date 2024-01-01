68d5a53a230f4c41d001f75db502a477984cfd71
hymntaha <tahauygun@gmail.com>
2024-01-01 22:18:04 +0000

    Adding memory buffer

--- app.py
+++ app.py
@@ -4,19 +4,25 @@ load_dotenv(find_dotenv(), override=True)
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage
 from langchain.chains import LLMChain
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate
+from langchain.memory import ConversationBufferMemory
 
 llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=1)
+
+memory = ConversationBufferMemory(memory_key='chat_history',
+                                  return_messages=True)
+
 prompt = ChatPromptTemplate(
     input_variables = ['content'],
     messages=[
        SystemMessage(content='You are chatbot having a conversation with a human.'), 
+       MessagesPlaceholder(variable_name='chat_history'),
        HumanMessagePromptTemplate.from_template('{content}')
     
     ]
 )
 
-chain = LLMChain(llm=llm, prompt=prompt, verbose=False)
+chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=False)
 
 while True:
     content = input('Your prompt: ')
