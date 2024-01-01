c13250bb71a439a32f99755d656ba6cb6ab5812c
hymntaha <tahauygun@gmail.com>
2024-01-01 22:23:56 +0000

    Adding session in LLM

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage
 from langchain.chains import LLMChain
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
 
 llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=1)
 
+history = FileChatMessageHistory('chat_history.json')
+
 memory = ConversationBufferMemory(memory_key='chat_history',
+                                  chat_memory=history,
                                   return_messages=True)
 
 prompt = ChatPromptTemplate(
@@ -18,7 +21,6 @@ prompt = ChatPromptTemplate(
        SystemMessage(content='You are chatbot having a conversation with a human.'), 
        MessagesPlaceholder(variable_name='chat_history'),
        HumanMessagePromptTemplate.from_template('{content}')
-    
     ]
 )
 
