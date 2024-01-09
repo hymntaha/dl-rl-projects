9dcca2fe8ff8ab5705bc46ba70156fefc232b434
hymntaha <tahauygun@gmail.com>
2024-01-09 04:35:44 +0000

    setting up new project

---
+++ dog_breed.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import random
+import os
+from PIL import Image
+
+import torch
+from torchsummary import summary
+from torch import nn, optim
+from torch.functional import F
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from torch.autograd import Variable
+
+plt.style.use('seaborn-whitegrid')
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['font.sans-serif'] = 'Arial'
+plt.rcParams['text.color'] = 'black'
+plt.rcParams['axes.labelcolor']= 'black'
+plt.rcParams['xtick.color'] = 'black'
+plt.rcParams['ytick.color'] = 'black'
+plt.rcParams['font.size']=12
+
+SEED = 1234
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
