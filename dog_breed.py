efe2562aea8aeda681cd78b8608011c349b5d45e
hymntaha <tahauygun@gmail.com>
2024-01-11 06:49:33 +0000

    Adding encoder

--- dog_breed.py
+++ dog_breed.py
@@ -37,3 +37,59 @@ labels = pd.read_csv(os.path.join(data_dir, 'labels.csv'))
 assert(len(os.listdir(os.path.join(data_dir, 'train'))) == len(labels))
 
 print(labels.head())
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+labels.breed = le.fit_transform(labels.breed)
+labels.head()
+
+print(labels.head())
+
+X = labels.id
+y = labels.breed
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, y,test_size=0.4, random_state=SEED, stratify=y)
+X_valid, X_test, y_valid, y_test = train_test_split(X_valid, y_valid, test_size=0.5, random_state=SEED, stratify=y_valid)
+
+class Dataset_Interpreter(Dataset):
+    def __init__(self, data_path, file_names, labels=None, transforms=None):
+        self.data_path = data_path
+        self.file_names = file_names
+        self.labels = labels
+        self.transforms = transforms
+        
+    def __len__(self):
+        return (len(self.file_names))
+    
+    def __getitem__(self, idx):
+        img_name = f'{self.file_names.iloc[idx]}.jpg'
+        full_address = os.path.join(self.data_path, img_name)
+        image = Image.open(full_address)
+        label = self.labels.iloc[idx]
+        
+        if self.transforms is not None:
+            image = self.transforms(image)
+            
+        return np.array(image), label
+    
+    def plot_images(images):
+
+        n_images = len(images)
+
+        rows = int(np.sqrt(n_images))
+        cols = int(np.sqrt(n_images))
+
+        fig = plt.figure(figsize=(20,10))
+        for i in range(rows*cols):
+            ax = fig.add_subplot(rows, cols, i+1)
+            ax.set_title(f'{le.inverse_transform([images[i][1]])}')
+            ax.imshow(np.array(images[i][0]))
+            ax.axis('off')
+        
+N_IMAGES = 9
+
+train_data = Dataset_Interpreter(data_path=data_dir+'train/', file_names=X_train, labels=y_train)
+images = [(image, label) for image, label in [train_data[i] for i in range(N_IMAGES)]] 
+plot_images(images)
