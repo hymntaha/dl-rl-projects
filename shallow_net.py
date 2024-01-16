44d291fd5d6f1752ca0753ff504eb25b3eb61429
hymntaha <tahauygun@gmail.com>
2024-01-16 06:28:36 +0000

    Project setup

--- shallow_net.py
+++ shallow_net.py
@@ -1,6 +1,7 @@
 import numpy as np
 np.random.seed(42)
 
+import tensorflow as tf
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
@@ -8,3 +9,6 @@ from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+print(X_train.shape)
+print(y_train.shape)
