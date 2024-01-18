5803bddadc8f3efc2e389c6a7015a7b5d7e4c5f9
hymntaha <tahauygun@gmail.com>
2024-01-18 06:06:49 +0000

    comment toxicity

---
+++ comment.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+
+df = pd.read_csv(os.path.join('data', 'train.csv', 'train.csv'))
+
+# print(df.head())
+
+from tensorflow.keras.layers import TextVectorization
+X = df['comment_text']
+y = df[df.columns[2:]].values
+
+print(df.columns)
+
+MAX_FEATURES = 200000
+
+vectorizer = TextVectorization(max_tokens=MAX_FEATURES, output_sequence_length=1800, output_mode='int')
+
+vectorizer.adapt(X.values)
+
+print(vectorizer("Hello world, life is great!"))
+
+vectorizer.get_vocabulary()
+
+vectorizer_text = vectorizer(X.values)
+print(vectorizer_text)
+
+dataset = tf.data.Dataset.from_tensor_slices((vectorizer_text, y))
+dataset = dataset.cache()
+dataset = dataset.shuffle(160000)
+dataset = dataset.batch(16)
+dataset = dataset.prefetch(8)
+
+print(dataset.as_numpy_iterator().next())
