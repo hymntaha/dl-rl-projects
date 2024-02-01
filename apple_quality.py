fa2d1ee96c927366dbdffb1caae16a7a47351b52
hymntaha <tahauygun@gmail.com>
2024-02-01 04:57:14 +0000

    Correlation & outliers

--- apple_quality.py
+++ apple_quality.py
@@ -69,3 +69,150 @@ def clean_data(df):
 
 df_clean = clean_data(df.copy())
 df_clean.head()
+
+numerical_cols = ['Size', 'Weight', 'Sweetness', 'Crunchiness', 'Juiciness', 'Ripeness', 'Acidity', 'Label']
+
+plt.figure(figsize=(15, 10))
+sns.set_palette("tab10")
+
+for i, column in enumerate(numerical_cols, 1):
+    plt.subplot(3, 3, i)
+    sns.histplot(data=df_clean, x=column, kde=True, bins=20)  # Use sns.histplot for newer versions
+    plt.title(column)
+
+plt.tight_layout()
+plt.show()
+
+numerical_cols = ['Size', 'Weight', 'Sweetness', 'Crunchiness', 'Juiciness', 'Ripeness', 'Acidity','Label']
+
+plt.figure(figsize=(15, 10))
+sns.set_palette("Set1")
+for i, column in enumerate(numerical_cols, 1):
+    plt.subplot(3, 3, i)
+    sns.boxplot(y=df_clean[column])
+    plt.title(column)
+
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(15, 10))
+sns.set_palette("Set2")
+
+for i, column in enumerate(df_clean.columns[:-1]):  
+    plt.subplot(3, 3, i + 1)
+    sns.boxplot(x='Label', y=column, data=df_clean)
+    plt.title(f'{column} by Label')
+
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(8, 8))
+sns.set(style="white")  
+
+sns.jointplot(x='Size', y='Weight', hue='Label', data=df_clean, palette='tab10', s=9)
+
+
+plt.figure(figsize=(8, 8))
+sns.set(style="white")  
+
+sns.jointplot(x='Sweetness', y='Crunchiness', hue='Label', data=df_clean, palette='tab10', s=9)
+
+plt.figure(figsize=(8, 8))
+sns.set(style="white")  
+
+sns.jointplot(x='Ripeness', y='Juiciness', hue='Label', data=df_clean, palette='tab10', s=9)
+
+corr = df_clean.corr()
+
+mask = np.triu(np.ones_like(corr, dtype=bool))
+
+plt.figure(figsize=(15, 12))
+
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5}, annot=True)
+
+plt.title('Correlation Heatmap')
+plt.show()
+
+numerical_features = ['Size', 'Weight', 'Sweetness', 'Crunchiness', 'Juiciness', 'Ripeness',
+       'Acidity']
+
+robust_scaler = RobustScaler()
+
+df_clean[numerical_features] = robust_scaler.fit_transform(df_clean[numerical_features])
+
+def count_outliers(series):
+    Q1 = series.quantile(0.25)
+    Q3 = series.quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return ((series < lower_bound) | (series > upper_bound)).sum()
+
+for feature in numerical_features :
+    num_outliers = count_outliers(df_clean[feature])
+    print(f'Number of outliers in {feature}: {num_outliers}')
+
+
+scaler = StandardScaler()
+
+df_clean[numerical_features] = scaler.fit_transform(df_clean[numerical_features])
+
+sns.set(style="whitegrid")
+
+
+def hypothesis_testing(df, column1, column2):
+    t_stat, p_val = stats.ttest_ind(df[column1], df[column2])
+    
+    print("T-statistic:", t_stat)
+    print("P-value:", p_val)
+    
+   
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x=df[column1], y="Variable", data=pd.melt(df[[column1, column2]], var_name="Variable"))
+    plt.title(f'Distribution of {column1} and {column2}')
+    plt.show()
+
+columns_for_testing = ['Size', 'Weight', 'Sweetness', 'Crunchiness', 'Juiciness', 'Ripeness', 'Acidity']
+
+for i in range(len(columns_for_testing) - 1):
+    hypothesis_testing(df_clean, columns_for_testing[i], columns_for_testing[i + 1])
+
+good_count = df_clean['Label'].eq(0).sum()
+bad_count = df_clean['Label'].eq(1).sum()
+total_count = len(df_clean)
+
+good_percentage = (good_count / total_count) * 100
+bad_percentage = (bad_count / total_count) * 100
+
+label_counts = df_clean['Label'].value_counts()
+
+plt.figure(figsize=(14, 6))
+
+# Subplot 1 - Pie Chart
+plt.subplot(1, 2, 1)
+pie_values = [good_percentage, bad_percentage]
+colors = ['darkgrey', 'lightgreen']
+plt.pie(pie_values, labels=['Good', 'Bad'],
+        autopct='%1.2f%%',
+        startangle=90,
+        explode=(0.1, 0.1),
+        colors=colors,
+        wedgeprops={'edgecolor': 'black', 'linewidth': 1, 'antialiased': True})
+plt.title('Imbalance Distribution')
+
+# Subplot 2 - Bar Chart
+plt.subplot(1, 2, 2)
+colors = ['darkgrey', 'lightgreen']
+ax = sns.barplot(x=label_counts.index, y=label_counts.values, palette=colors, edgecolor='black')
+for i in ax.patches:
+    ax.annotate(f'{i.get_height()}', (i.get_x() + i.get_width() / 2., i.get_height()),
+                ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+ax.set_xticks([0, 1])  
+ax.set_xticklabels(['Good', 'Bad'])
+plt.title('Count of Observations in Each Cluster')
+
+plt.tight_layout()
+plt.show()
